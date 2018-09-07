b81d29890eac9ce2dc775b23c6707d66ef15c19b
sergey petrachkov <petrachkovsergey@gmail.com>
2018-09-07 14:52:36 +0700

    Initial Commit

---
+++ StretchyScrollExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchyScrollExample
+//
+//  Created by Sergey Petrachkov on 07/09/2018.
+//  Copyright © 2018 Sergey Petrachkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
